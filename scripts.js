9211961606369f54e85e99788e96ef254a72e21f
Shiki Wang <thisisshiki@outlook.com>
2023-12-14 04:26:00 +1100

    added resize and clear functions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
-const gridSize = 16;
 const gridContainer = document.querySelector('.grid-container');
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.className = 'grid-item';
-    gridContainer.appendChild(gridItem);
-  }
+function setSize(num) {
+    let gridSize = num;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridContainer.appendChild(gridItem);
+        gridItem.style.width = `calc(100% / ${gridSize})`;
+        gridItem.style.height = `calc(100% / ${gridSize})`;
+        gridItem.addEventListener('mouseover', changeColor);
+      }
+    }
+setSize(16);
+
+const resize = document.querySelector('#resize');
+resize.addEventListener('click', () => {
+    gridSize = prompt('Enter new grid size:');
+    gridContainer.innerHTML = '';
+    setSize(gridSize);
+});
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((gridItem) => {
+        gridItem.style.backgroundColor = 'white';
+    });
+});
+
+
